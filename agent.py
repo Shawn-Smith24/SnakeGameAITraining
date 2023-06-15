62394b26e00442944e7e16e0f5769e78807201d9
shawn <shawnsmith0024@gmail.com>
2023-06-15 11:28:09 -0400

    training object set up

--- agent.py
+++ agent.py
@@ -30,7 +30,7 @@ class Agent:
     def train_long_memory(self):
         pass
     
-    def train_short_memory(self):
+    def train_short_memory(self, state, action, reward, next_state, done):
         pass
     
     def get_action(self, state):
@@ -38,8 +38,43 @@ class Agent:
     
     
 def train():
-    pass
-
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakedGameAI()
+    while True:
+        # get old state
+        state_old = agent.get_state(game)
+        
+        #get move 
+        final_move = agent.get_action(state_old)
+        
+        #perform move and get new state
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+        
+        ##train short memory 
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+        
+        #remember all 
+        agent.remember(state_old, final_move, reward, state_new, done)
+        
+        if done:
+            #train the long memory, plo result 
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+            
+            if score > record:
+                record = score
+                #TODO agent.model,save()
+            
+            print('Game', agent.n_games, 'Score', score, 'Records', record)
+            #TODO plot 
+            
+            
 
 if __name__ == '__main__':
     train()
