7e82e928a936dc2619e3d9a82ac34fa9c2427bea
shawn <shawnsmith0024@gmail.com>
2023-06-15 11:30:03 -0400

    get_state set up

--- agent.py
+++ agent.py
@@ -1,7 +1,7 @@
-import torch 
-import random 
-import numpy as np 
-from collections import deque #data structure to store memories 
+import torch
+import random
+import numpy as np
+from collections import deque  # data structure to store memories
 from game import SnakedGameAI, Direction, Point
 
 
@@ -10,18 +10,60 @@ BATCH_SIZE = 1000
 LR = 0.001
 
 
-class Agent: 
-    
+class Agent:
+
     def __init__(self):
         self.n_games = 0
-        self.epsilon = 0 #controls randomness
-        self.gamma = 0 #discount rate
-        self.memory = deque(maxlen=MAX_MEMORY) # calls popleft() for us 
-        # TODO: mdoel, trainer
-    
-    
+        self.epsilon = 0  # controls randomness
+        self.gamma = 0  # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY)  # calls popleft() for us
+        # TODO: model, trainer
+
     def get_state(self, game):
-        pass
+        head = game.snake[0]
+        point_l = Point(head.x - 20, head.y)
+        point_r = Point(head.x + 20, head.y)
+        point_u = Point(head.x, head.y - 20)
+        point_d = Point(head.x, head.y + 20)
+        
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+
+        state = [
+            # Danger straight
+            (dir_r and game.is_collision(point_r)) or 
+            (dir_l and game.is_collision(point_l)) or 
+            (dir_u and game.is_collision(point_u)) or 
+            (dir_d and game.is_collision(point_d)),
+
+            # Danger right
+            (dir_u and game.is_collision(point_r)) or 
+            (dir_d and game.is_collision(point_l)) or 
+            (dir_l and game.is_collision(point_u)) or 
+            (dir_r and game.is_collision(point_d)),
+
+            # Danger left
+            (dir_d and game.is_collision(point_r)) or 
+            (dir_u and game.is_collision(point_l)) or 
+            (dir_r and game.is_collision(point_u)) or 
+            (dir_l and game.is_collision(point_d)),
+            
+            # Move direction
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+            
+            # Food location 
+            game.food.x < game.head.x,  # food left
+            game.food.x > game.head.x,  # food right
+            game.food.y < game.head.y,  # food up
+            game.food.y > game.head.y  # food down
+            ]
+
+        return np.array(state, dtype=int)
     
     def remember(self, state, action, reward, next_state, done):
         pass
